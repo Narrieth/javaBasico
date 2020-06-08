2e80a26e02d673cbe4ff710162e2af19819817a7
narrieth <narrieth.felipe@salamancasolution.com>
2020-06-08 18:51:04 -0400

    hola mundo

---
+++ HolaMundo/src/main/java/paquete/Main.java
@@ -0,0 +1,12 @@
+package paquete;
+
+/**
+ *
+ * @author Narrieth Felipe Peñaranda
+ */
+public class Main {
+    public static void main (String[] args){
+        //TODO
+        System.out.println("Hola Mundo");
+    }
+}
