4e24dba4d367ef1e99294a69187cb9a1db35ed75
narrieth <narrieth.felipe@salamancasolution.com>
2020-06-11 17:42:50 -0400

    ciclo for

---
+++ CicloFor/src/main/java/paquete/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete;
+
+/**
+ *
+ * @author Narrieth Felipe Peñaranda
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        for (int contador = 1; contador < 20; contador++) {
+//            contador = contador * 2;
+            System.out.println("El contador vale: " + contador);
+            if (contador == 19) {
+                System.out.println("ultimo elemento: " + contador);
+            }
+        }
+
+//        for (int i = 0; i < accordeon; i++) {
+//            if (i == accordeon - 1) {
+//                // Instrucciones de la ultima iteración
+//            }
+//        }
+
+    }
+
+}
