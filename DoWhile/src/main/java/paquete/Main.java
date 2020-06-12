e4fe7e71546660ab0db3c2988cf4b1329e7612e8
narrieth <narrieth.felipe@salamancasolution.com>
2020-06-11 20:42:10 -0400

    instruccion while

---
+++ DoWhile/src/main/java/paquete/Main.java
@@ -0,0 +1,24 @@
+package paquete;
+
+/**
+ *
+ * @author Narrieth Felipe Peñaranda
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 0;
+        boolean cond = true;
+
+//        while (cond) {
+        while (x <= 15) {
+            System.out.println("El valor de x es: " + x);
+            x = (x*2)+3;
+
+//            if (x > 8) {
+//                cond = false;
+//            }
+        }
+    }
+
+}
