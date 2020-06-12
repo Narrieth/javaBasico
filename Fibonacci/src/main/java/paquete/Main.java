0290a83439840586c5485c8f61270361030ad66e
narrieth <narrieth.felipe@salamancasolution.com>
2020-06-12 17:46:02 -0400

    ciclos, serie de numeros impares y fibonacci

--- Fibonacci/src/main/java/paquete/Main.java
+++ Fibonacci/src/main/java/paquete/Main.java
@@ -13,12 +13,26 @@ public class Main {
 
     public static void main(String[] args) {
         //algoritmos ciclicos
-        //serie de fibonacci 0,1,1,2,3,5,8,11,...
-        
+
         //serie de numeros impares
-        for (int i = 0; i <= 100; i++) {
-            if ((i % 2) != 0) {
-                System.out.println(i);
+//        for (int i = 0; i <= 100; i++) {
+//            if ((i % 2) != 0) {
+//                System.out.println(i);
+//            }
+//        }
+        //serie de fibonacci 0,1,1,2,3,5,8,11,...
+        int x = 1;
+        int anterior = 0;
+        int temp;
+
+        while (true) {
+            System.out.println(x);
+            temp = x;
+            x = x + anterior;
+            anterior = temp;
+            
+            if (x > 100) {
+                break;
             }
         }
     }
