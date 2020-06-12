b96df4a94e404d9dbcb923c0960288a325e5c14d
narrieth <narrieth.felipe@salamancasolution.com>
2020-06-12 13:01:41 -0400

    ciclos serie de numeros impares

---
+++ Fibonacci/src/main/java/paquete/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete;
+
+/**
+ *
+ * @author Narrieth Felipe Peñaranda
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //algoritmos ciclicos
+        //serie de fibonacci 0,1,1,2,3,5,8,11,...
+        
+        //serie de numeros impares
+        for (int i = 0; i <= 100; i++) {
+            if ((i % 2) != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
